6b8c8fc75803e774f22e1d31e47fa30a4c1430f3
Ronee <mataupana10@gmail.com>
2023-10-04 16:37:08 +0200

    Challenge 1

---
+++ DWA5/scripts.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+
+  if (dividend.trim() === "" || divider.trim() === "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again"
+  } else if (divider < 0) {
+    result.innerText = "Division not performed. Invalid number provided. Try again"
+    throw new Error("Division not performed") 
+  } else if (isNaN(dividend) || isNaN(divider)) {
+    document.body.innerHTML  = "Something critical went wrong. Please reload the page"
+    throw new Error("Something critical went wrong") 
+  } else {
+    result.innerText = Math.floor(dividend / divider);
+  }
+});
+
+
