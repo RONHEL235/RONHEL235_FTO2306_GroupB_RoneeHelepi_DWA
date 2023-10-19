ec26f35be2034a0bdfeed27ebff6c58ef101a5e8
Ronee <mataupana10@gmail.com>
2023-10-19 07:11:26 +0200

    Update DWA9

--- DWA9/scripts.js
+++ DWA9/scripts.js
@@ -14,7 +14,6 @@ let matches = books //Array of book objects
  * @returns {HTMLButtonElement} - A button element representing the book with its details.
  */
 
-// script.js
 class BookComponent extends HTMLElement {
     constructor() {
       super();
@@ -25,8 +24,60 @@ class BookComponent extends HTMLElement {
       const { author, id, image, title } = this.bookData;
       this.shadowRoot.innerHTML = `
         <style>
-          .preview {
-            /* Your styles here */
+        .preview {
+            border-width: 0;
+            width: 100%;
+            font-family: Roboto, sans-serif;
+            padding: 0.5rem 1rem;
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+            text-align: left;
+            border-radius: 8px;
+            border: 1px solid rgba(var(--color-dark), 0.15);
+            background: rgba(var(--color-light), 1);
+          }
+          
+          @media (min-width: 60rem) {
+            .preview {
+              padding: 1rem;
+            }
+          }
+          
+          .preview_hidden {
+            display: none;
+          }
+          
+          .preview:hover {
+            background: rgba(var(--color-blue), 0.05);
+          }
+          
+          .preview__image {
+            width: 48px;
+            height: 70px;
+            object-fit: cover;
+            background: grey;
+            border-radius: 2px;
+            box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+              0px 1px 1px 0px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+          }
+          
+          .preview__info {
+            padding: 1rem;
+          }
+          
+          .preview__title {
+            margin: 0 0 0.5rem;
+            font-weight: bold;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;  
+            overflow: hidden;
+            color: rgba(var(--color-dark), 0.8)
+          }
+          
+          .preview__author {
+            color: rgba(var(--color-dark), 0.4);
           }
         </style>
         <button class="preview" data-preview="${id}">
@@ -66,9 +117,6 @@ class BookComponent extends HTMLElement {
     dataListItems.appendChild(bookItem);
   });
   
-  
-
-
 
 /**
  * Creates an HTML <option> element with specified value and text content. 
