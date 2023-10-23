3133f74747067f00a48b852364ec3cebc8c7d580
Ronee <mataupana10@gmail.com>
2023-10-23 17:17:18 +0200

    Update DWA11

--- DWA11/store.js
+++ DWA11/store.js
@@ -1,5 +1,10 @@
 // Redux setup
 import { createStore } from 'redux'
+//State
+const initialState = {
+    counter: 0
+}
+
 
 // Actions
 const increment = () => {
@@ -15,26 +20,26 @@ const reset = () => {
 }
   
 // Reducer
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'RESET':
-      return state = 0
-    default:
-      return state
-  }
-}
-
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { ...state, counter: state.counter + 1 };
+        case 'DECREMENT':
+            return { ...state, counter: state.counter - 1 };
+        case 'RESET':
+            return { ...state, counter: 0 };
+        default:
+            return state;
+    }
+};
 // Create a Redux store
 const store = createStore(counterReducer)
 
 // Subscribe to the store to listen for changes
-store.subscribe(() => console.log('Current count:', store.getState()))
+store.subscribe(() => console.log('Current count:', store.getState().counter))
 
-// Dispatch actions to update the state
+// Dispatch actions to update the counter
+console.log('Current count:', store.getState().counter)
 store.dispatch(increment())
 store.dispatch(increment())
 store.dispatch(increment())
